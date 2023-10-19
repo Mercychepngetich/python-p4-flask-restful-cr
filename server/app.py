4d7de49a0fea8029caaf16271e12f97e9388cbb6
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-10-20 01:12:58 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -17,7 +17,61 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+   def get(self):
+       newsletter_list=[]
+       newsletters = Newsletter.query.all()
+       for newsletter in newsletters:
+           newsletterObj={
+               "id":newsletter.id,
+               "title":newsletter.title,
+               "body":newsletter.body,
+               "published_at":newsletter.published_at,
+               "edited_at":newsletter.edited_at
+
+           }
+           newsletter_list.append(newsletterObj)
+       return jsonify(newsletter_list)
+   
+   def post(self):
+       new_newsletter = Newsletter(
+           title = request.form['title'],
+           body = request.form['body']
+       )
+
+       db.session.add(new_newsletter)
+       db.session.commit()
+       response_dict = new_newsletter.to_dict()
+       response_body = {
+          'Message': "Newslettter created successfully",
+          'status':201, 
+          'dict': response_dict
+       }
+       response = make_response(response_body,200)
+       return response
+       
+api.add_resource(Home, '/newsletters') 
+
+class NewsletterByID(Resource):
+    def get(self,id):
+        newsletter = Newsletter.query.filter_by(id=id).first()
+        newsletter_dict = newsletter.to_dict()
+
+        response_body ={
+            "dict":newsletter_dict,
+            "status":200
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+        return response
+
+        
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
+    
+   
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
